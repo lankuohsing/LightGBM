f95f1eabe0e04363ef216fb974734cec64097112
l00467141 <languoxing@huawei.com>
2018-10-18 15:18:02 +0800

    add example and 1

--- examples/lambdarank/lambdarank_example.py
+++ examples/lambdarank/lambdarank_example.py
@@ -18,15 +18,15 @@ f = open("./rank.train.query","r",encoding='UTF-8')   #设置文件对象
 qid_train_count_list = f.readlines()  #直接将文件中按行读到list里
 f.close()             #关闭文
 # In[]
-for i in range(len(qid_train_count_list)):
-    qid_train_count_list[i]=int(qid_train_count_list[i])
+#for i in range(len(qid_train_count_list)):
+#    qid_train_count_list[i]=int(qid_train_count_list[i])
 # In[]
 f = open("./rank.test.query","r",encoding='UTF-8')   #设置文件对象
 qid_test_count_list = f.readlines()  #直接将文件中按行读到list里
 f.close()             #关闭文
 # In[]
-for i in range(len(qid_test_count_list)):
-    qid_test_count_list[i]=int(qid_test_count_list[i])
+#for i in range(len(qid_test_count_list)):
+#    qid_test_count_list[i]=int(qid_test_count_list[i])
 # In[]
 lgb_train = lgb.Dataset(X_train, y_train,group=qid_train_count_list)
 lgb_eval = lgb.Dataset(X_test, y_test,group=qid_test_count_list)
@@ -37,8 +37,11 @@ params={
 print('Start training...')
 # train
 gbm = lgb.train(params=params,train_set=lgb_train)
+#print('Start training...')
+#gbm=lgb.Booster(model_file='lambdarank_example.model')
 # In[]
 print('Start predicting...')
 # predict
 prob_pred_lgb = gbm.predict(X_test,config="predict.conf")
 # In[]
+gbm.save_model("lambdarank_example_model.txt")
--- examples/lambdarank/lambdarank_example1.py
+++ examples/lambdarank/lambdarank_example1.py
@@ -9,60 +9,30 @@ Created on Tue Sep 25 17:18:07 2018
 
 from sklearn.datasets import load_svmlight_file
 import lightgbm as lgb
+import os,sys
+from sklearn.datasets import load_svmlight_file
 # In[]
+#查看当前工作目录
+print("当前的工作目录为：%s" %os.getcwd())
 # In[]
 """获取训练集的qid"""
 f = open("./Fold1/trainingset.txt","r",encoding='UTF-8')   #设置文件对象
 train_list = f.readlines()  #直接将文件中按行读到list里
 f.close()             #关闭文件
-qid_train_list=[]
-for i in range(len(train_list)):
-    qid_train_i=int(train_list[i].split(" ")[1].split(":")[1])
-    qid_train_list.append(qid_train_i)
 # In[]
-qid_train_set=set(qid_train_list)
+X_train,y_train=load_svmlight_file("data_train.txt")
+X_test,y_test=load_svmlight_file("data_test.txt")
 # In[]
-qid_train_count_list=[]
+f = open("data_train_group.txt","r",encoding='UTF-8')
+data_group_train = f.readlines()
+f.close()
 # In[]
-for qid_train in qid_train_set:
-    qid_train_count=qid_train_list.count(qid_train)
-    qid_train_count_list.append(qid_train_count)
+f = open("data_test_group.txt","r",encoding='UTF-8')
+data_group_test = f.readlines()
+f.close()
 # In[]
-with open("./Fold1/trainingset.txt.query","w") as f:
-    for qid_count in qid_train_count_list:
-        f.write(str(qid_count))
-        f.write("\n")
-    print("加载入文件完成...")
-# In[]
-# In[]
-"""获取测试集的qid"""
-f = open("./Fold1/testset.txt","r",encoding='UTF-8')   #设置文件对象
-test_list = f.readlines()  #直接将文件中按行读到list里
-f.close()             #关闭文件
-qid_test_list=[]
-for i in range(len(test_list)):
-    qid_test_i=int(test_list[i].split(" ")[1].split(":")[1])
-    qid_test_list.append(qid_test_i)
-# In[]
-qid_test_set=set(qid_test_list)
-# In[]
-qid_test_count_list=[]
-# In[]
-for qid_test in qid_test_set:
-    qid_test_count=qid_test_list.count(qid_test)
-    qid_test_count_list.append(qid_test_count)
-# In[]
-with open("./Fold1/testset.txt.query","w") as f:
-    for qid_count in qid_test_count_list:
-        f.write(str(qid_count))
-        f.write("\n")
-    print("加载入文件完成...")
-# In[]
-X_train, y_train = load_svmlight_file("./Fold1/trainingset.txt")
-X_test,y_test=load_svmlight_file("./Fold1/testset.txt")
-# In[]
-lgb_train = lgb.Dataset(X_train, y_train,group=qid_train_count_list)
-lgb_eval = lgb.Dataset(X_test, y_test,group=qid_test_count_list)
+lgb_train = lgb.Dataset(X_train, y_train,group=data_group_train)
+lgb_eval = lgb.Dataset(X_test, y_test,group=data_group_test)
 params={
         "application":"lambdarank",
         "boosting_type": "gbdt",
@@ -75,30 +45,17 @@ print('Start training...')
 # train
 gbm = lgb.train(params,
                 lgb_train)
+gbm.save_model("lambdarank_example_model1.txt")
 # In[]
 print('Start predicting...')
 # predict
 prob_pred_lgb = gbm.predict(X_test)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # In[]
-#clf_lgb = lgb.LGBMClassifier(application='multiclass',boosting='gbdt',max_depth=6,num_leaves ='10',min_data_in_leaf=10)
-
-# In[]
-#clf_lgb.fit(X_train, y_train,eval_set=[(X_test, y_test)])
 
 
-# In[]
+
+
+
+
+
+
